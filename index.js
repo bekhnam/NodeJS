786861242bcba619628cf88500c520aa1c8a5975
Ngo Thanh Nam <ngothanhnam2016@gmail.com>
2021-08-04 16:00:17 +0300

    Mongoose Part 2

--- index.js
+++ index.js
@@ -8,24 +8,22 @@ const connect = mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopolog
 connect.then((db) => {
     console.log("Connected correctly to server");
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+        return Dishes.deleteMany({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-            return Dishes.find({})
-        })
-        .then((dishes) => {
-            console.log(dishes);
-            return Dishes.deleteMany({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 });
